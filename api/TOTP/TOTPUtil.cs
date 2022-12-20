a3485312a9d39e3d3561e58f4e8f4fa148f0d3cc
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-12-20 13:25:30 +0100

    whitepsace

--- api/TOTP/TOTPUtil.cs
+++ api/TOTP/TOTPUtil.cs
@@ -9,14 +9,14 @@ namespace BulbasaurAPI.TOTP
             byte[] key = KeyGeneration.GenerateRandomKey(20);
             string base32String = Base32Encoding.ToString(key);
             byte[] secret = Base32Encoding.ToBytes(base32String);
-
             return secret;
         }
 
         internal static bool VerifyTOTP(byte[] TOTPSecret, string totpCode)
         {
             var totp = new Totp(TOTPSecret);
-            return totp.VerifyTotp(totpCode, out long timeWindowUsed);
+            bool result = totp.VerifyTotp(totpCode, out long timeWindowUsed);
+            return result;
         }
     }
 }
