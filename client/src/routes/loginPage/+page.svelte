c810357169935f46bb22d2bcc8c7c02033e55419
Patricia Serti <patricia_serti@hotmail.com>
2023-01-10 10:21:26 +0100

    +page för login

---
+++ client/src/routes/loginPage/+page.svelte
@@ -0,0 +1,18 @@
+
+
+<p class="step1">Logga in på Skolplaneten </p>
+
+
+
+
+<style lang="less">
+	//@import 'public\less\variables.less';
+	//@import 'public\less\global.less';    
+	.k {
+		color: none;
+		border: none;
+	}
+
+	
+</style>
+
