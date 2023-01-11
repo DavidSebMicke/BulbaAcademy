e1404313ea7a2a612cb2c02911872467d7d0492c
DanielFrones <daniel.frones@gmail.com>
2023-01-11 10:46:34 +0100

    Adding repo

---
+++ api/Repository/GroupRepository.cs
@@ -0,0 +1,59 @@
+﻿using BulbasaurAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace BulbasaurAPI.Repository
+{
+    public class GroupRepository : IGroupRepository
+    {
+
+        private readonly DbServerContext _context;
+
+        public GroupRepository(DbServerContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Group> CreateGroupAsync()
+        {
+            //var groups = _context.Groups.ToListAsync();
+            //return groups;
+            throw new NotImplementedException();
+
+        }
+
+        
+        public async Task<bool> DeleteGroupAsync(int id)
+        {
+           var delete = await _context.Groups.Where(x=> x.Id == id).FirstOrDefaultAsync();
+             _context.Remove(delete);
+            return await SaveAsync();
+           
+           
+        }
+
+        public async Task<IEnumerable<Group>> GetAllGroupsAsync()
+        {
+            return await _context.Groups.ToListAsync();
+        }
+
+        public async Task<Group> GetGroupByIdAsync(int id)
+        {
+            return await _context.Groups.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            var saved = _context.SaveChangesAsync();
+            return await saved > 0 ? true : false;
+        }
+
+        public Task UpdateGroupAsync(Group group)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- api/Repository/IGroupRepository.cs
+++ api/Repository/IGroupRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace BulbasaurAPI.Repository
+﻿using BulbasaurAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulbasaurAPI.Repository
 {
     public interface IGroupRepository
     {
+
+        Task<Group> GetGroupByIdAsync(int id);
+
+        Task<IEnumerable<Group>> GetAllGroupsAsync();   
+
+        Task<Group> CreateGroupAsync();
+
+        Task<bool> DeleteGroupAsync(int id);   
+
+        Task UpdateGroupAsync(Group group);
+
+        Task<bool> SaveAsync();
+
     }
 }
