54b93fb49e0dcda68f816cd416761bbfd19d8229
Patricia Serti <patricia_serti@hotmail.com>
2023-01-12 17:50:08 +0100

    updates

--- client/src/routes/loginPage/+page.svelte
+++ client/src/routes/loginPage/+page.svelte
@@ -1,18 +1,36 @@
 
-
-<p class="step1">Logga in på Skolplaneten </p>
-
-
-
-
-<style lang="less">
-	//@import 'public\less\variables.less';
-	//@import 'public\less\global.less';    
-	.k {
-		color: none;
-		border: none;
+<p class="step1">Logga in på Bulba Academy</p>
+<script>
+	
+	function handleClick() {
+		alert('Du är inloggad')
+		
 	}
 
+	</script>
+
+
+	<div class="step2">
+
+	<p>Användarnamn<p><input value="">
+	<p>Lösenord</p><input value="">
+
+	
+</div>
+<div>
+	<img src="public\img\bulba.png" alt="gfdkl">
+</div>
+<button on:click={handleClick}>
+	Logga in
+</button>
+
+<style lang="less">
+	@import 'public\less\variables.less';
+	@import 'public\less\global.less'; 
+	  
+	.test {
+		color: @second-sec-color;
+		border: none;
+	}
 	
 </style>
-
