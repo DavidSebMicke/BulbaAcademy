856b46a220da319cf17dafa342e47bf5829011f7
Patricia Serti <patricia_serti@hotmail.com>
2023-01-13 00:18:41 +0100

    ny bild + styling i global.less

--- client/src/routes/loginPage/+page.svelte
+++ client/src/routes/loginPage/+page.svelte
@@ -1,36 +1,39 @@
-
-<p class="step1">Logga in på Bulba Academy</p>
 <script>
 	
 	function handleClick() {
 		alert('Du är inloggad')
 		
 	}
+	
+</script>
 
-	</script>
 
-
+<div class="griden">
+	<div id="step1">Logga in i Bulba Academy</div>
+	
 	<div class="step2">
 
-	<p>Användarnamn<p><input value="">
-	<p>Lösenord</p><input value="">
-
+	<p>Användarnamn<br><input value="">
+	<p>Lösenord<br><input value="">
+		<br><br>
+		
+		<button id="step3" on:click={handleClick}>
+			<h1>Logga in</h1>
+		</button>
+	</div>
+	
+	<img class="bulben" src="public\img\bulbi.jpg" alt="gfdkl">
 	
 </div>
-<div>
-	<img src="public\img\bulba.png" alt="gfdkl">
-</div>
-<button on:click={handleClick}>
-	Logga in
-</button>
+
+
 
 <style lang="less">
 	@import 'public\less\variables.less';
 	@import 'public\less\global.less'; 
-	  
-	.test {
-		color: @second-sec-color;
-		border: none;
-	}
+	@import url('https://fonts.googleapis.com/css2?family=Poiret+One&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
 	
 </style>
