ccc72718e7e683801d3923a9754ac710d3cb5bcb
Gaztly <89972155+Gaztly@users.noreply.github.com>
2023-01-16 19:59:34 +0100

    Fixed global.less

--- client/src/components/dashboard/dashboard.svelte
+++ client/src/components/dashboard/dashboard.svelte
@@ -93,7 +93,7 @@
 		justify-content: left;
 		height: 100em;
 		color: currentColor;
-		background-color: purple;
+		background-color: #e1b382;
 		text-decoration: none;
 		float: left;
 		padding: 10px;
