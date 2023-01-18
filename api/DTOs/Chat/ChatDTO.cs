6934aeae9ceae53bd75a2730b331c9313dc00b3b
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2023-01-18 11:47:04 +0100

    Added Chat controller functionality with DTOs

---
+++ api/DTOs/Chat/ChatDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace BulbasaurAPI.DTOs.Chat
+{
+    public class ChatDTO
+    {
+        public int ChatId { get; set; }
+        public List<ChatUserDTO> Users { get; set; }
+        public List<ChatMessageDTO> Messages { get; set; }
+
+        public ChatDTO(Models.Chat chat)
+        {
+            ChatId = chat.Id;
+            Users = new();
+            Messages = new();
+
+            chat.InvolvedUsersList.ForEach(user =>
+            {
+                Users.Add(new ChatUserDTO(user));
+            });
+
+            chat.ChatItemList.ForEach(chatItem =>
+            {
+                Messages.Add(new ChatMessageDTO(chatItem));
+            });
+        }
+    }
+}
