2fd510e97483146718f1de291cac4bd2516546b5
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2023-01-27 10:13:39 +0100

    minor fix for group fetch in document repository

--- api/Repository/DocumentRepository.cs
+++ api/Repository/DocumentRepository.cs
@@ -36,7 +36,7 @@ namespace BulbasaurAPI.Repository
             return await _context.Documents
                 .Include(d => d.UploadedBy)
                 .Include(d => d.EligibleList)
-                .Include(d => d.EligibleGroups).ThenInclude(g => g.Users)
+                .Include(d => d.EligibleGroups).ThenInclude(g => g.People)
                 .ToListAsync();
         }
 
