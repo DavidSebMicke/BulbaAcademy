019de7c9865a2b6e207c97ebc93ccd727c0b6992
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2023-01-30 14:18:02 +0100

    Added custom validation attributes for data annotation
    Added åäö functionality to the frontend validators

---
+++ api/DataAnnotations/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BulbasaurAPI.DataAnnotations
+{
+    public class PhoneNumber : ValidationAttribute
+    {
+        // Checks if the phonenumber is valid (removes - and spaces before testing).
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string strVal = (string)value;
+
+            strVal.Replace('-', string.Empty.ToCharArray()[0]);
+            strVal.Replace(' ', string.Empty.ToCharArray()[0]);
+
+            string pattern = @"(0|\+)[0-9]+";
+
+            var match = Regex.Match(strVal, pattern);
+
+            return match.Success ?
+                ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
