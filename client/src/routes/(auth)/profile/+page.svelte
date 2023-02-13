8dc325bc1324c6fc74459f9e778ef94fd091a023
davidKael <70914882+davidKael@users.noreply.github.com>
2023-02-13 17:45:20 +0100

    printsoutProfileDataInProfile

--- client/src/routes/(auth)/profile/+page.svelte
+++ client/src/routes/(auth)/profile/+page.svelte
@@ -1,8 +1,17 @@
 <script>
+	import { onMount } from 'svelte';
 	import Header from '../../../components/header/header.svelte';
+	import { user } from '../../../stores/userStores';
+
 </script>
 
 <Header title="Profile" />
+
+<div>Epost: {$user?.email}</div>
+<div>
+	 Namn: {#if $user?.name}{$user?.name}{:else}Ej Registrerad{/if}	{#if $user?.role}({$user?.role}){/if}
+</div>
+<div></div>
 
 <style lang="less">
 </style>
