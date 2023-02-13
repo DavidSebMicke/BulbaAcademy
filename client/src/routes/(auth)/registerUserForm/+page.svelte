ff5a4f91a98e24f7355bb9802221d1c21dc4a031
Sloththeswift <71036969+Sloththeswift@users.noreply.github.com>
2023-02-13 10:19:42 +0100

    more

--- client/src/routes/(auth)/registerUserForm/+page.svelte
+++ client/src/routes/(auth)/registerUserForm/+page.svelte
@@ -1,4 +1,14 @@
 <script>
+	import { useForm, Hint, HintGroup, validators, required, number } from 'svelte-use-form';
+	import {
+		SSNCheck,
+		emailCheck,
+		RequiredMsg,
+		streetAddressCheck,
+		phoneNumberCheck,
+		cityCheck,
+		postCodeCheck
+	} from '$Utils/Validation';
 </script>
 
 <form>
@@ -10,8 +20,36 @@
 		<label for="email"> E-postadress</label>
 		<input type="mail" name="email" label="email" placeholder="Skriv in din e-postadress" />
 	</div>
-	<button class="classic-button" type="submit">Registrera</button>
+	<span class="subButt">
+		<button class="classic-button" type="submit">Registrera</button>
+	</span>
 </form>
 
 <style lang="less">
+	.form-group {
+		justify-content: center;
+		height: 100em;
+		width: 30em;
+		position: absolute;
+		flex-wrap: nowrap;
+		margin-bottom: 40%;
+		left: 40%;
+		height: 100%;
+		top: 10;
+		display: flex;
+		align-items: left;
+		justify-content: left;
+		flex-direction: column;
+		margin-bottom: 4%;
+	}
+	.subButt {
+		width: 95%;
+		margin-bottom: 3%;
+		cursor: pointer;
+		position: relative;
+		font-size: 20px !important;
+		margin-bottom: 20%;
+		align-self: flex-end;
+		top: 100;
+	}
 </style>
