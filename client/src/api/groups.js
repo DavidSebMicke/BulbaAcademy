1a40481f6f5a96d91ed32b3f13ba328e8e66c2b2
davidKael <70914882+davidKael@users.noreply.github.com>
2023-02-15 08:52:04 +0100

    frontend group call

--- client/src/api/groups.js
+++ client/src/api/groups.js
@@ -15,3 +15,25 @@ export async function GetAllGroups() {
 		throw err;
 	}
 }
+
+export async function GetAllMembersInGroup(groupId) {
+	const endpoint = '/People/GetAllPersonsByGroupId';
+	try {
+		const response = await api.get(endpoint, {
+			params: {
+				groupId: groupId
+			}
+		});
+
+		if (response.status == 200) {
+			return response.data;
+		} else {
+			return false;
+		}
+		
+	} catch (err) {
+		console.error(err);
+
+		throw err;
+	}
+}
